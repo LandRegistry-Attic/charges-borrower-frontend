fb1205e9d1320f11453142f1e6cb798986ed820e
Michael Allen <michael@michaelallen.io>
2015-05-08 13:25:13 +0100

    switch to a systemd service

--- puppet/borrower_frontend/manifests/init.pp
+++ puppet/borrower_frontend/manifests/init.pp
@@ -6,12 +6,12 @@ class borrower_frontend {
     source   => 'git://github.com/LandRegistry/charges-borrower-frontend',
     revision => 'master'
   }
-  file { '/etc/init.d/borrower_frontend':
+  file { '/etc/init.d/borrower_frontend.service':
     ensure => 'file',
     mode   => '0755',
     owner  => 'root',
     group  => 'root',
-    source => "puppet:///modules/${module_name}/borrower_frontend_service.sh",
+    source => "puppet:///modules/${module_name}/borrower_frontend.service",
     notify => Service['borrower_frontend'],
   }
   service { 'borrower_frontend':
