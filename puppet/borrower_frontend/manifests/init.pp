0a1bedc0b494464bc337bee5bdbef45ace3f6f34
Michael Allen <michael@michaelallen.io>
2015-05-18 17:41:33 +0100

    missed sbin reference

--- puppet/borrower_frontend/manifests/init.pp
+++ puppet/borrower_frontend/manifests/init.pp
@@ -41,7 +41,7 @@ class borrower_frontend ($port = '9000', $host = '0.0.0.0') {
     provider => 'systemd',
     require  => [
       Vcsrepo['/opt/borrower-frontend'],
-      File['/opt/borrower-frontend/sbin/run.sh'],
+      File['/opt/borrower-frontend/bin/run.sh'],
       File['/etc/init.d/borrower_frontend'],
       File['/etc/systemd/system/borrower_frontend.service']
     ],
