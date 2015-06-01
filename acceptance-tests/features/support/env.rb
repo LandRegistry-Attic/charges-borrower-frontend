3eb1715b21ced00e674639b191cffd4a24ad5640
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-06-01 12:46:46 +0100

    updated feature and step defs based on error handling changes

--- acceptance-tests/features/support/env.rb
+++ acceptance-tests/features/support/env.rb
@@ -5,4 +5,4 @@
 ################################################################################
 
 $HELLOWORLD_DOMAIN = (ENV['HELLOWORLD_DOMAIN'] || 'http://localhost:5000')
-$CHARGES_URL = (ENV['CHARGES_URL'] || 'http://localhost:5000')
+$CHARGES_URL = (ENV['CHARGES_URL'] || 'http://0.0.0.0:5000')
