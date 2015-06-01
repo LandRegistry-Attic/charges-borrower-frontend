2038804af65947e444dc9d066922bbcdb0a350b4
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-06-01 11:55:58 +0100

    Styles updated

--- app/static/pipeline.py
+++ app/static/pipeline.py
@@ -5,13 +5,14 @@ import sass as libsass
 
 __dot = path.dirname(path.realpath(__file__))
 __toolkit_dir = path.join(__dot, 'govuk_frontend_toolkit/stylesheets/')
+__elemets_dir = path.join(__dot, 'sass/elements/')
 
 
 def __compile_sass(_in, out, **kw):
     out.write(
         libsass.compile(
             string=_in.read(),
-            include_paths=[__toolkit_dir]
+            include_paths=[__toolkit_dir, __elemets_dir]
         )
     )
 
