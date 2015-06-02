d8d0354718d4a375144ff19400633ab2fef412be
Michael Allen <michael@michaelallen.io>
2015-06-02 12:21:06 +0100

    run the app as a user rather than root

--- puppet/borrower_frontend/manifests/init.pp
+++ puppet/borrower_frontend/manifests/init.pp
@@ -27,6 +27,11 @@ class borrower_frontend (
     require => Vcsrepo['/opt/borrower-frontend'],
     notify  => Service['borrower_frontend'],
   }
+  file { '/var/run/borrower_frontend':
+    ensure => 'directory',
+    owner  => $owner,
+    group  => $group,
+  }
   file { '/etc/nginx/conf.d/borrower_frontend.conf':
     ensure  => 'file',
     mode    => '0755',
@@ -58,7 +63,8 @@ class borrower_frontend (
       Vcsrepo['/opt/borrower-frontend'],
       File['/opt/borrower-frontend/bin/run.sh'],
       File['/etc/init.d/borrower_frontend'],
-      File['/etc/systemd/system/borrower_frontend.service']
+      File['/etc/systemd/system/borrower_frontend.service'],
+      File['/var/run/borrower_frontend'],
     ],
   }
 }
