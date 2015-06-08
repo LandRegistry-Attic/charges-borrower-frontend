50cb87e55f40af7f518743bec161e6d513ae4171
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-06-08 17:41:04 +0100

    fixed issue unable to find rubocop.yml file

--- acceptance-tests/run_linting.sh
+++ acceptance-tests/run_linting.sh
@@ -11,6 +11,6 @@ BUNDLE_GEMFILE=$gemfile bundle install
 
 ### Run rubocop gem to check acceptance tests against the Ruby style guide.
 ### Will fail with exit code if any errors are raised
-BUNDLE_GEMFILE=$gemfile bundle exec rubocop -c rubocop.yml
+BUNDLE_GEMFILE=$gemfile bundle exec rubocop -c $currentLocation/rubocop.yml
 
 exit $?
