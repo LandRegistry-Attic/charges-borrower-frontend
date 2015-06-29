45c5bfe883383799fd46a4e6ad1f2b05d616f50a
cs112mm <mandy.nimmo@digital.landregistry.gov.uk>
2015-06-29 16:11:07 +0100

    amended step defs to check from created data

--- acceptance-tests/features/steps/borrower_views_deed_definitions.rb
+++ acceptance-tests/features/steps/borrower_views_deed_definitions.rb
@@ -16,46 +16,34 @@ Then(/^the Sign your mortgage deed page is displayed$/) do
 end
 
 Then(/^the title number appears on the page$/) do
-  assert_selector(".//*[@id='title-number']", text: 'GHR67832')
+  assert_selector(".//*[@id='title-number']", text: @deed['deed']['operative-deed']['title']['title-number'])
 end
 
 Then(/^the property address appears on the page$/) do
-  assert_selector("//*[@id='property-address']", text: 'Flat 16 Kingman Court')
+  assert_selector("//*[@id='property-address']", text: @deed['deed']['operative-deed']['title']['address'])
 end
 
 Then(/^the borrower name appears on the page$/) do
-  assert_selector("//*[@id='borrower-name-0']", text: 'Peter Smith')
+  assert_selector("//*[@id='borrower-name-0']", text: @deed['deed']['operative-deed']['borrowers'])
 end
 
 Then(/^the borrower address appears on the page$/) do
   assert_selector("//*[@id='borrower-address-0']",
-                  text: 'Flat 16 Kingman Court')
+                  text: @deed['deed']['operative-deed']['borrowers']['address'])
 end
 
 Then(/^the lender name appears on the page$/) do
-  assert_selector("//*[@id='lender-name']", text: 'Bank of England Plc')
+  assert_selector("//*[@id='lender-name']", text: @deed['deed']['operative-deed']['lender']['lender-name'])
 end
 
 Then(/^the charging clause appears on the page$/) do
   assert_selector("//*[@id='clauses-and-provisions']/*",
-                  text: 'You, the borrower, with full title guarantee, charge'\
-                  ' property to the lender by way of legal mortgage with the'\
-                  ' payment of all money secured by this charge.')
+                  text: @deed['deed']['operative-deed']['charging-clause'])
 end
 
 Then(/^the additional provisions appear on the page$/) do
   assert_selector("//*[@id='clauses-and-provisions']/*",
-                  text: 'This Mortgage Deed incorporates the Lenders Mortgage'\
-                  ' Conditions and Explanation 2006, a copy of which has been'\
-                  ' received by the Borrower.')
-  assert_selector("//*[@id='clauses-and-provisions']/*",
-                  text: 'The lender is under an obligation to make further'\
-                  ' advances and applies for the obligation to be entered in'\
-                  ' the register.')
-  assert_selector("//*[@id='clauses-and-provisions']/*",
-                  text: 'No disposition of the registered estate by the'\
-                  ' proprietor of the registered estate is to be registered'\
-                  ' without a written consent signed by Bank of England Plc.')
+                  text: @deed['deed']['operative-deed']['provisions'])
 end
 
 Then(/^the Invalid deed reference page is displayed$/) do
