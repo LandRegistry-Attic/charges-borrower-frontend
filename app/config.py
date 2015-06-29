a13a64e949f32f9f3e2eddbd3f20a3351f84c88a
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-06-29 10:25:51 +0100

    unnecessary vars removed

--- app/config.py
+++ app/config.py
@@ -2,5 +2,3 @@ import os
 
 DEBUG = True
 DEED_API_BASE_HOST = os.getenv('DEED_API_ADDRESS', 'http://localhost:5050')
-# DEED_CLIENT = "app.service.deed_api_client.DeedApiClient"
-DEED_CLIENT = 'tests.mock.deed_api_mock_client.DeedApiMockClient'
