83139cdf918d43055f32d1a9b7ab1648693656da
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-07-07 12:00:42 +0100

    sign deed endpoint changed

--- app/service/scribe_api/implementation.py
+++ app/service/scribe_api/implementation.py
@@ -5,8 +5,6 @@ SCRIBE_API_BASE_HOST = config.SCRIBE_API_BASE_HOST
 
 
 def sign_deed(deed_id, borrower_id, borrower_name):
-    json_body = {"deed_id": deed_id,
-                 "borrower_id": borrower_id,
-                 "borrower_name": borrower_name}
-    url = SCRIBE_API_BASE_HOST + "/deed/signature"
+    json_body = {"borrower_name": borrower_name}
+    url = SCRIBE_API_BASE_HOST + "/deed/" + deed_id + "/" + borrower_id + "/" + "signature"
     return requests.post(url, json=json_body)
