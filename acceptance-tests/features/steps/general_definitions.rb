b5cf7ab89a7f76ed1231bd827f208b22a222495b
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-07-10 14:59:59 +0100

    fixed issues with sign test using hardcoded url

--- acceptance-tests/features/steps/general_definitions.rb
+++ acceptance-tests/features/steps/general_definitions.rb
@@ -7,6 +7,10 @@ Given(/^I navigate to the borrower frontend "([^"]*)" page$/) do |path|
   visit($BORROWER_FRONTEND_URL + path)
 end
 
+When(/^I click on the "([^"]*)" button$/) do |button_name|
+  click_button(button_name)
+end
+
 Then(/^the "([^"]*)" page is displayed$/) do |page_title|
   expect(page).to have_content(page_title)
 end
