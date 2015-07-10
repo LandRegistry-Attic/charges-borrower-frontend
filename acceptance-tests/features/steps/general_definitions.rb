a1c0052ed817151d0c9606b4fa8951110561f425
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-07-10 09:37:28 +0100

    added acceptance test scenario skeletons

---
+++ acceptance-tests/features/steps/general_definitions.rb
@@ -0,0 +1,11 @@
+Given(/^I navigate to the borrower frontend "([^"]*)" page$/) do |path|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on the "([^"]*)" link$/) do |link_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the "([^"]*)" page is displayed$/) do |page_title|
+  pending # Write code here that turns the phrase above into concrete actions
+end
