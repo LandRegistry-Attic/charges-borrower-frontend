45481d581dcc4551e6c7ba64f77441af9c8d300c
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-07-21 14:14:30 +0100

    cleanup

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 DEBUG = True
-DEED_API_BASE_HOST = os.getenv('DEED_API_ADDRESS', 'http://10.10.10.10:5000')
-SCRIBE_API_BASE_HOST = os.getenv('SCRIBE_API_ADDRESS', 'http://localhost:6000')
+DEED_API_BASE_HOST = os.getenv('DEED_API_ADDRESS', '')
+SCRIBE_API_BASE_HOST = os.getenv('SCRIBE_API_ADDRESS', '')
