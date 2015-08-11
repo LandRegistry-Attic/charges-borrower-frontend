0ea3dc330dec1a2373306a5c0ac9864f183e04e0
Michael Allen <michael@michaelallen.io>
2015-08-11 18:55:09 +0100

    Pass the current tests, prev commit broke them

--- app/deed/sign/routes.py
+++ app/deed/sign/routes.py
@@ -1,11 +1,14 @@
 from app.deed.sign import views
-from flask import redirect, url_for, request, abort, make_response
+from flask import redirect, url_for, request, abort
 
 
 def register_routes(blueprint, scribe_api, deed_api):
     @blueprint.route('/deed/sign/confirmation', methods=['GET'])
     def confirmation():
-        return views.Confirmation().render()
+        deed_id = request.cookies.get('deed_id')
+        signed_details = deed_api.get_signed_status(deed_id)
+
+        return views.Confirmation(None, signed_details).render()
 
     @blueprint.route('/deed/sign', methods=['POST'])
     def sign_deed():
@@ -16,10 +19,6 @@ def register_routes(blueprint, scribe_api, deed_api):
         response = scribe_api.sign_deed(deed_id, borrower_id, borrower_name)
 
         if response.status_code == 200:
-            signed_details = deed_api.get_signed_status(deed_id)
-            views_page = views.View(signed_details).render()
-
-            response = make_response(views_page)
             return redirect(url_for('deed.sign.confirmation'), code=303)
         else:
             abort(response.status_code)
